1137812e586065177ab2d14f091b1ebbd7e2817a
Gabe Yamartino <gabeyamartino@gmail.com>
2022-02-25 18:15:23 -0500

    finalized some styling things, calling it good for now.

--- etch.js
+++ etch.js
@@ -1,8 +1,4 @@
 const container = document.querySelector('#container');
-let cell = document.createElement('div');
-cell.classList.add('columns');
-let resetButton = document.getElementById('reset');
-
 
 function createGrid(num) {
     for (i = 0; i < num; i++) {
@@ -14,7 +10,11 @@ function createGrid(num) {
             cell.classList.add('columns');
             rows.appendChild(cell);
             cell.addEventListener('mouseover', function() {
-                cell.style.backgroundColor = 'blue';  
+                let x = Math.floor(Math.random() * 256);
+                let y = Math.floor(Math.random() * 256);
+                let z = Math.floor(Math.random() * 256);
+                let rainbow = "rgb(" + x + "," + y + "," + z + ")";
+                cell.style.backgroundColor = rainbow;  
             });
         }
     }
@@ -22,33 +22,12 @@ function createGrid(num) {
 
 createGrid(16);
 
+let resetButton = document.getElementById('reset');
 resetButton.addEventListener("click", resetGrid);
 
 function resetGrid() {
     document.getElementById('container').innerHTML = "";
     let x = prompt("Please type a number between 1-100");
     createGrid(x);
-}
-// the code below is bullshit.
-// look it over & find out why.
-
-let coloredCells = document.querySelectorAll('div.columns');
-console.log(coloredCells.length)
-
-let newCell;
-function changeColor() {
-    cell.style.backgroundColor = 'blue';
-}
-
-coloredCells.forEach((newCell) => {
-    newCell.addEventListener('mouseover', changeColor);
-});
-console.log(newCell)
-
-// crapy example of a for loop to add listeners
-
-for (i = 0; i < frog.length; i++) {
-    frog[i].addEventListener
-}
-
+};
 
